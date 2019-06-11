1007e34b035f1b3a8c7df524810ec6b3ea556559
David Morris <dnmorris7@gmail.com>
2019-06-11 14:09:55 -0400

    Module2 Initial Changes

--- src/main/java/com/davidonus/secureapp/AppSecurityConfig.java
+++ src/main/java/com/davidonus/secureapp/AppSecurityConfig.java
@@ -3,20 +3,33 @@ package com.davidonus.secureapp;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends 	WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsService userDetailService;
 	
 	@Bean
+	public AuthenticationProvider authProvider(){
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	}
+	
 	@Override
 	public UserDetailsService userDetailsService() {
 	
@@ -31,5 +44,4 @@ public class AppSecurityConfig extends 	WebSecurityConfigurerAdapter {
 		
 		//TODO @Test: What if any of these attributes are empty?
 	}
-
 }
